nmathewson/libevent_obsolete

Cleaner shutdown funtionality.
- Added function libevent_shutdown() which calls a set of private functions:
       * event_free_globals()
       * event_free_debug_globals()
       * event_free_debug_globals_locks()
       * event_free_evsig_globals()
       * evsig_free_globals()
       * evsig_free_globals_locks()
       * evutil_free_globals()
       * evutil_free_secure_rng_globals()
       * evutil_free_secure_rng_globals_lock()

event.c
@@ -2822,6 +2822,45 @@ event_base_del_virtual(struct event_base *base)
 	EVBASE_RELEASE_LOCK(base, th_base_lock);
 }
 
+static void
+event_free_debug_globals_locks(void) {
+#ifndef _EVENT_DISABLE_THREAD_SUPPORT
+#ifndef _EVENT_DISABLE_DEBUG_MODE
+	if (_event_debug_map_lock != NULL) {
+		EVTHREAD_FREE_LOCK(_event_debug_map_lock, 0);
+	}
+#endif /* _EVENT_DISABLE_DEBUG_MODE */
+#endif /* _EVENT_DISABLE_THREAD_SUPPORT */
+	return;
+}
+
+static void
+event_free_debug_globals(void) {
+	event_free_debug_globals_locks();
+}
+
+static void
+event_free_evsig_globals(void) {
+	evsig_free_globals();
+}
+
+static void
+event_free_evutil_globals(void) {
+	evutil_free_globals();
+}
+
+static void
+event_free_globals(void) {
+	event_free_debug_globals();
+	event_free_evsig_globals();
+	event_free_evutil_globals();
+}
+
+void
+libevent_shutdown(void) {
+	event_free_globals();
+}
+
 #ifndef _EVENT_DISABLE_THREAD_SUPPORT
 int
 event_global_setup_locks_(const int enable_locks)

signal.c
@@ -433,6 +433,21 @@ evsig_dealloc(struct event_base *base)
 	}
 }
 
+static void
+evsig_free_globals_locks(void) {
+#ifndef _EVENT_DISABLE_THREAD_SUPPORT
+	if (evsig_base_lock != NULL) {
+		EVTHREAD_FREE_LOCK(evsig_base_lock, 0);
+	}
+#endif
+	return;
+}
+
+void
+evsig_free_globals(void) {
+	evsig_free_globals_locks();
+}
+
 #ifndef _EVENT_DISABLE_THREAD_SUPPORT
 int
 evsig_global_setup_locks_(const int enable_locks)
@@ -440,4 +455,5 @@ evsig_global_setup_locks_(const int enable_locks)
 	EVTHREAD_SETUP_GLOBAL_LOCK(evsig_base_lock, 0);
 	return 0;
 }
+
 #endif

childhood/Libevent

event.c
@@ -3124,6 +3124,51 @@ event_base_del_virtual_(struct event_base *base)
 	EVBASE_RELEASE_LOCK(base, th_base_lock);
 }
 
+static void
+event_free_debug_globals_locks(void)
+{
+#ifndef EVENT__DISABLE_THREAD_SUPPORT
+#ifndef EVENT__DISABLE_DEBUG_MODE
+	if (event_debug_map_lock_ != NULL) {
+		EVTHREAD_FREE_LOCK(event_debug_map_lock_, 0);
+	}
+#endif /* _EVENT_DISABLE_DEBUG_MODE */
+#endif /* _EVENT_DISABLE_THREAD_SUPPORT */
+	return;
+}
+
+static void
+event_free_debug_globals(void)
+{
+	event_free_debug_globals_locks();
+}
+
+static void
+event_free_evsig_globals(void)
+{
+	evsig_free_globals_();
+}
+
+static void
+event_free_evutil_globals(void)
+{
+	evutil_free_globals_();
+}
+
+static void
+event_free_globals(void)
+{
+	event_free_debug_globals();
+	event_free_evsig_globals();
+	event_free_evutil_globals();
+}
+
+void
+libevent_shutdown(void)
+{
+	event_free_globals();
+}
+
 #ifndef EVENT__DISABLE_THREAD_SUPPORT
 int
 event_global_setup_locks_(const int enable_locks)

signal.c
@@ -449,11 +449,27 @@ evsig_dealloc_(struct event_base *base)
 	}
 }
 
+static void
+evsig_free_globals_locks(void) {
 #ifndef EVENT__DISABLE_THREAD_SUPPORT
+	if (evsig_base_lock != NULL) {
+		EVTHREAD_FREE_LOCK(evsig_base_lock, 0);
+	}
+#endif
+	return;
+}
+
+#ifndef EVENT__DISABLE_THREAD_SUPPORT
+void
+evsig_free_globals_(void) {
+	evsig_free_globals_locks();
+}
+
 int
 evsig_global_setup_locks_(const int enable_locks)
 {
 	EVTHREAD_SETUP_GLOBAL_LOCK(evsig_base_lock, 0);
 	return 0;
 }
+
 #endif